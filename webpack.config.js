cbaaeced49ba99bef9911103f075d5bfbc3df282
ndemin <niko.demin@gmail.com>
2022-01-16 00:58:56 +0300

    restore after lost data

--- webpack.config.js
+++ webpack.config.js
@@ -6,28 +6,33 @@ module.exports = {
         import: './src/main/resources/static/ts/app.tsx',
         dependOn: 'shared',
       },
+      api: {
+        import: './src/main/resources/static/ts/api.ts',
+        dependOn: 'shared',
+      },
       transformers_table: {
         import: './src/main/resources/static/ts/transformers-table.tsx',
         dependOn: 'shared',
       },
       shared: 'lodash',
      },
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
+    devtool: 'inline-source-map',
+    cache: true,
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
     },
-    module: {
-        rules: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
-                }]
-            }
-        ]
+    output: {
+      path: __dirname,
+      filename: './src/main/resources/static/built/[name].bundle.js',
+      chunkFilename: './src/main/resources/static/built/[name].chunk.js'
+    },
+     module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
     }
 };
