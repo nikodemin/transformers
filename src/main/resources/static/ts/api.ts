8b0989356d29219dea3551c4d6bc6db6af0ef766
ndemin <niko.demin@gmail.com>
2022-01-17 01:02:02 +0300

    almost finished crud on transformers

--- src/main/resources/static/ts/api.ts
+++ src/main/resources/static/ts/api.ts
@@ -1,6 +1,8 @@
-import axios, {AxiosResponse, AxiosResponseHeaders, AxiosResponseTransformer} from "axios"
+import axios, {AxiosResponse, AxiosResponseTransformer} from "axios"
 
 const BasePath = 'http://localhost:8080/api/'
+
+// Transformer:
 
 export interface Transformer {
     id: number
@@ -11,9 +13,7 @@ export interface Transformer {
     baseId: number
 }
 
-const transformersResponseTransformer = (axios.defaults.transformResponse as AxiosResponseTransformer[]).concat(
-    (data: any, headers?: AxiosResponseHeaders) => data._embedded.transformers
-)
+const transformersResponseTransformer = responseTransformer((data) => data._embedded.transformers)
 
 export function deleteTransformersByIdIn(ids: number[], handleResp: (resp: any) => void, onError: (err: any) => void = err => undefined) {
     axios.delete(BasePath + 'transformers?ids=' + ids.join(',')).then(handleResp).catch(onError)
@@ -26,5 +26,32 @@ export function getTransformers(handleResp: (resp: AxiosResponse<Transformer[], 
 }
 
 export function addTransformer(transformer: Transformer, handleResp: (resp: AxiosResponse<Transformer, any>) => void, onError: (err: any) => void = err => undefined) {
-    axios.post<Transformer>(BasePath + 'transformers').then(handleResp).catch(onError)
+    axios.post<Transformer>(BasePath + 'transformers', transformer).then(handleResp).catch(onError)
+}
+
+// Base:
+
+export interface Base {
+    id: number
+    name: string
+    location: Location
+}
+
+const baseResponseTransformer = responseTransformer((data) => data._embedded.bases)
+
+export function getBases(handleResp: (resp: AxiosResponse<Base[], any>) => void, onError: (err: any) => void = err => undefined) {
+    axios.get<Base[]>(BasePath + "bases", {
+        transformResponse: baseResponseTransformer
+    }).then(handleResp).catch(onError)
+}
+
+// Common
+
+export interface Location {
+    latitude: number
+    longitude: number
+}
+
+function responseTransformer(transformer: AxiosResponseTransformer) {
+    return (axios.defaults.transformResponse as AxiosResponseTransformer[]).concat(transformer)
 }
--- src/main/resources/static/ts/transformers-table.tsx
+++ src/main/resources/static/ts/transformers-table.tsx
@@ -1,43 +1,52 @@
-import {Container, Dialog, DialogContent, DialogTitle, FormControl, Input, InputLabel, Stack} from '@mui/material';
+import {Container, Dialog, DialogContent, DialogTitle, MenuItem, Select, Stack, TextField} from '@mui/material';
 import Button from '@mui/material/Button';
 import {DataGrid, GridCallbackDetails, GridColDef, GridSelectionModel} from '@mui/x-data-grid';
 import {FormEvent, useReducer, useState} from 'react';
-import {deleteTransformersByIdIn, getTransformers, Transformer} from './api';
+import {addTransformer, Base, deleteTransformersByIdIn, getBases, getTransformers, Transformer} from './api';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
 
 
 const columns: GridColDef[] = [
     {
         field: 'id',
         headerName: 'id',
-        type: 'number'
+        type: 'number',
+        width: 100
     },
     {
         field: 'name',
         headerName: 'Name',
-        editable: true
+        editable: true,
+        flex: 0.8
     },
     {
         field: 'dateOfBuild',
         headerName: 'Date of build',
-        editable: true
+        editable: true,
+        flex: 1
     },
     {
         field: 'height',
         headerName: 'Height',
         type: 'number',
-        editable: true
+        editable: true,
+        flex: 0.6
     },
     {
         field: 'weight',
         headerName: 'Weight',
         type: 'number',
-        editable: true
+        editable: true,
+        flex: 0.6
     },
     {
         field: 'baseId',
         headerName: 'Base ID',
         type: 'number',
-        editable: true
+        editable: true,
+        width: 100
     }
 ]
 
@@ -46,11 +55,15 @@ export default function () {
     const [rows, setRows] = useState<Transformer[]>([])
     const [selectedRows, setSelectedRows] = useState<any[]>([])
     const [showAddDialog, setShowAddDialog] = useState(false)
+    const [addDate, setAddDate] = useState<Date | null>(null);
+    const [addBaseId, setAddBaseId] = useState<number | null>(null)
+    const [addBases, setAddBases] = useState<Base[]>([])
     const [formInput, setFormInput] = useReducer(
         (state: any, newState: any) => ({...state, ...newState}),
         {
             name: "",
-            weight: 0
+            weight: 0,
+            height: 0
         }
     );
 
@@ -67,35 +80,54 @@ export default function () {
         setSelectedRows(rows.filter(r => model.includes(r.id)))
     }
 
-    function handleInput(evt: any) {
-        const name = evt.target.id;
-        const newValue = evt.target.value;
+    function handleInput(evt: any, isNumber: boolean = false) {
+        const name = evt.target.name;
+        const newValue = isNumber ? +evt.target.value : evt.target.value;
         setFormInput({[name]: newValue});
     }
 
-    function add(e: FormEvent) {
-        console.log(formInput)
+    function updateBases() {
+        getBases(resp => setAddBases(resp.data))
+    }
+
+    function addTransformerHandler(e: FormEvent) {
+        e.preventDefault()
+        let transformer = {...formInput, ...{baseId: addBaseId, dateOfBuild: addDate?.toISOString()}} as Transformer
+        console.log(transformer)
+        addTransformer(transformer, () => null)
     }
 
     return (
-        <Stack spacing={1} height="500px">
+        <Stack spacing={1} height="500px" maxWidth={1200}>
             <Container maxWidth={false}>
                 <Button onClick={reload}>Reload</Button>
                 <Button onClick={() => setShowAddDialog(true)}>Add new</Button>
                 <Button onClick={deleteRows}>Delete selected</Button>
             </Container>
-            <DataGrid rows={rows} columns={columns} pageSize={5} onSelectionModelChange={handleSelected}/>
+            <DataGrid rows={rows} columns={columns} pageSize={5} onSelectionModelChange={handleSelected}
+                      checkboxSelection={true}/>
             <Dialog open={showAddDialog} onClose={() => setShowAddDialog(false)}>
-                <DialogTitle>Subscribe</DialogTitle>
+                <DialogTitle>Add transformer</DialogTitle>
                 <DialogContent>
-                    <form onSubmit={add}>
-                        <FormControl>
-                            <InputLabel htmlFor="name">Name: </InputLabel>
-                            <Input id="name" name="Name" onInput={handleInput}/>
-                            <InputLabel htmlFor="weight">Weight: </InputLabel>
-                            <Input id="weight" name="Weight" type="number" onInput={handleInput}/>
+                    <form onSubmit={addTransformerHandler}>
+                        <Stack spacing={1}>
+                            <TextField name="name" label="Name" onInput={handleInput}/>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <DatePicker
+                                    value={addDate}
+                                    label="Date of build"
+                                    onChange={(newValue) => setAddDate(newValue)}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                            <TextField name="height" label="Height" type="number" onInput={e => handleInput(e, true)}/>
+                            <TextField name="weight" label="Weight" type="number" onInput={e => handleInput(e, true)}/>
+                            <Select value={addBaseId} onOpen={updateBases}
+                                    onChange={e => setAddBaseId(e.target.value as number)}>
+                                {addBases.map(base => <MenuItem value={base.id}>{base.name}</MenuItem>)}
+                            </Select>
                             <Button type="submit">Add</Button>
-                        </FormControl>
+                        </Stack>
                     </form>
                 </DialogContent>
             </Dialog>
